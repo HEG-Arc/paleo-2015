eefdfe770ae1e1ff1f1a895f2285cc929e497b98
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2015-05-07 18:31:56 +0200

    4 buttons uhid reader

---
+++ daemon/uhid_manager.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+# uhid_manager.py
+#
+# Copyright (C) 2015 HES-SO//HEG Arc
+#
+# Author(s): Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
+#
+# This file is part of Gestion'air 2015.
+#
+# Gestion'air 2015 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gestion'air 2015 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gestion'air 2015. If not, see <http://www.gnu.org/licenses/>.
+
+
+def send_code(code):
+    return "Code: %s" % code
