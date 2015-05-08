7aa777ecdaeeda0fd3f8551bf4f7bf8e2607f3bd
bvuille <b.vuille@gmail.com>
2015-05-08 13:50:32 +0200

    suppression package business et modification fichiers .py

--- gestionair/command/urls.py
+++ gestionair/command/urls.py
@@ -4,3 +4,15 @@ except ImportError:  # django < 1.4
     from django.conf.urls.defaults import *
 
 # place app url patterns here
+
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+
+urlpatterns = patterns('gestionair.command.views',
+    url(r'^home$', 'home'),
+    url(r'^temps_attente$', 'temps_attente'),
+)
--- gestionair/command/views.py
+++ gestionair/command/views.py
@@ -1,1 +1,28 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+
+
+def home(request):
+    text = """<h1>Bienvenue sur le Gestion'air !</h1>"""
+    return HttpResponse(text)
+
+def temps_attente(request):
+    return redirect("url")
+
+def resultat(request):
+    return redirect("url")
+
+def compte_a_rebours(request):
+    return redirect("url")
+
+def temps_attente_html(request):
+    return redirect("url")
+
+def resultat_html(request):
+    return redirect("url")
+
+def compte_a_rebours(request):
+    return redirect("url")
